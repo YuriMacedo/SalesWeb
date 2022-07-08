717c5c4afc418717481343900ca6b1634e1c9a92
YuriMacedo <yuri_paulino@hotmail.com>
2022-07-08 12:10:51 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMvc/Models/Departament.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class Departament
+    {
+        public int id { get; set; }
+        public string  name { get; set; }
+
+        public Departament(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Departament()
+        {
+        }
+
+
+    }
+}
